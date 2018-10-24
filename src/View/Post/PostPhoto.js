0e535853614b0d95099da2ca11c7aa0024d2b112
vuthihuong <vuthihuong161996@gmail.com>
2018-10-24 09:09:44 +0700

    # check validate cho bài post photo

--- src/View/Post/PostPhoto.js
+++ src/View/Post/PostPhoto.js
@@ -6,57 +6,135 @@ import { Dropdown } from 'react-native-material-dropdown';
 import DatePicker from 'react-native-datepicker'
 import CheckBox from 'react-native-checkbox';
 import gobackIcon from './../../assets/img/info/goback.png'
+import {FirebaseApp} from './../../Controller/FirebaseConfig' 
 
 export default class PostPhoto extends Component {
     constructor(props){
         super(props)
         this.state = {
-            date: '', 
-            time: '00:00',
-            datetimePhoto: '',
-            selectedHours: 0,
-            selectedMinutes: 0,
-            datetimePhoto1: '',
-            selectedHours1: 0,
-            selectedMinutes1: 0,
-            contentPhoto: '', costPhoto:''
+            date: '', time: '00:00', datetimePhoto: '', selectedHours: 0, selectedMinutes: 0,
+            datetimePhoto1: '', selectedHours1: 0, selectedMinutes1: 0, contentPhoto: '', costPhoto:'',
+            labelErrorAddress: false, labelErrorCatg: false, labelErrorCost: false, labelErrorTime: false,
+            valueCategoryPhoto1: '', valuePlacePhoto: ''
         }
+        this.itemRef = FirebaseApp.database();
       }
+
+      componentWillMount() {
+        tmp = FirebaseApp.auth().currentUser.email
+        FirebaseApp.database().ref('Customer').orderByChild("email").equalTo(tmp)
+                   .on('value', function (snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+                         key = childSnapshot.key;
+          })  
+        })
+        
+    }
 
     static navigationOptions = ({navigation}) => {
         const {params = {}} = navigation.state;
         let tabBarLabel = 'Tìm nháy ảnh';
         return {tabBarLabel}
     }
+    createPostPhoto(){ 
+        const reg =  /^\+?[0-9][\d]*$/;
+        if(this.state.valueCategoryPhoto1 === '' || this.state.valuePlacePhoto === '' 
+            || this.state.datetimePhoto === '' || this.state.datetimePhoto1 === '' || this.state.costPhoto === ''){
+                if(this.state.valueCategoryPhoto1 === ''){ 
+                    this.setState({ 
+                        labelErrorCatg: true
+                    })
+                }
+                else if(this.state.valueCategoryPhoto1 !== ''){ 
+                    this.setState({ 
+                        labelErrorCatg: false
+                    })
+                }
+                if(this.state.valuePlacePhoto === ''){ 
+                    this.setState({ 
+                        labelErrorAddress: true
+                    })
+                }
+                else if(this.state.valuePlacePhoto !== ''){ 
+                    this.setState({ 
+                        labelErrorAddress: false
+                    })
+                }
+                if(this.state.datetimePhoto === '' || this.state.datetimePhoto1 === ''){ 
+                    this.setState({ 
+                        labelErrorTime: true
+                    })
+                }
+                else if(this.state.datetimePhoto !== '' && this.state.datetimePhoto1 !== ''){ 
+                    this.setState({ 
+                        labelErrorTime: false
+                    })
+                }
+                if(this.state.costPhoto === '' || reg.test(this.state.costPhoto) === false ){ 
+                    this.setState({ 
+                        labelErrorCost: true
+                    })
+                }
+                else if(this.state.costPhoto !== '' && reg.test(this.state.costPhoto )=== true ){ 
+                    this.setState({ 
+                        labelErrorCost: false
+                    })
+                }
+            }
+        else if(this.state.valueCategoryPhoto1 !== '' && this.state.valuePlacePhoto !== "" && reg.test(this.state.costPhoto )=== true
+            && this.state.datetimePhoto !== '' && this.state.datetimePhoto1 !== '' && this.state.costPhoto !== ''){ 
+                this.itemRef.ref('PostPhoto').push({
+                    userId: key, title: "Tìm nháy ảnh" + this.state.valueCategoryPhoto1,
+                    contentPhoto: this.state.contentPhoto, costPhoto: this.state.costPhoto,
+                    datetimePhoto: this.state.datetimePhoto, datetimePhoto1: this.state.datetimePhoto1,
+                    valuePlacePhoto: this.state.valuePlacePhoto
+                   
+                    }).then((snap) => { this.setState({  
+                                                 id: snap.key })
+                         if(this.state.id !== ''){ 
+                            this.props.navigation.navigate('PostDetailPhoto',{
+                                id: this.state.id, userId: key,
+                                valueCategoryPhoto1: this.state.valueCategoryPhoto1,
+                                contentPhoto: this.state.contentPhoto,
+                                valuePlacePhoto: this.state.valuePlacePhoto,
+                                datetimePhoto: this.state.datetimePhoto,
+                                datetimePhoto1: this.state.datetimePhoto1,
+                                costPhoto: this.state.costPhoto,
+                            })
+                         }
+                         this.setState({ 
+                             id: '', valueCategoryPhoto1: '', valuePlacePhoto: '',
+                             contentPhoto: '', datetimePhoto: '', datetimePhoto1: '', costPhoto: ''
+                         })
+                    })
+               
+        }
+    
+}
 
-       render()
-       {
-        let data = [{
-            value: 'Hà Nội',
-          }, {
-            value: 'Hải Dương',
-          }, {
-            value: 'Hải Phòng',
-          }];
+    
 
-          let category = [{
-            value: 'Chụp ảnh cá nhân'
-          },
-          {
-            value: 'Chụp ảnh đôi'
-          },
-          {
-            value: 'Chụp ảnh nhóm'
-          },
-          {
-            value: 'Chụp ảnh kỷ yếu'
-          },
-          {
-            value: 'Chụp ảnh cưới'
-          },
-          {
-            value: 'Chụp ảnh quảng cáo, sự kiện'
-          }]
+       render()  {
+        var data = [];
+        FirebaseApp.database().ref("DataAddress/").on('value', (function (snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                let childData = childSnapshot.val();
+                data.push(childData)
+            });
+           
+        }))
+    
+
+        var category = [];
+        FirebaseApp.database().ref("DataCatgPostPhoto/").on('value', (function (snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                let childData = childSnapshot.val();
+                category.push(childData)
+            });
+           
+        }))
           const { selectedHours, selectedMinutes } = this.state;
           return(
             <ScrollView style={{flex:1, backgroundColor: 'white'}}>
@@ -74,6 +152,10 @@ export default class PostPhoto extends Component {
                             <View><Text></Text></View>
                             
                         </View>
+                        {this.state.labelErrorCatg === true?
+                            <View style={stylesPostPhoto.title}>
+                                <Text style={{color:'red'}}>Bạn chưa chọn thể loại</Text>
+                            </View>: null}
                         <View style={stylesPostPhoto.title}>
                             <Text style={{marginRight: 10, marginTop: 10,color:'black' }}>Thể loại</Text>
                             
@@ -92,17 +174,24 @@ export default class PostPhoto extends Component {
                                     onChangeText={(contentPhoto) => this.setState({contentPhoto})}>
                                     {this.state.contentPhoto}</TextInput>
                         </View>
+                        {this.state.labelErrorAddress === true ? 
+                             <View style={stylesPostPhoto.title}>
+                                    <Text style={{color: 'red'}}>Bạn chưa chọn địa điểm</Text>
+                             </View>: null}
                         <View style={stylesPostPhoto.title}>
-                            <Text style={{marginRight: 10, marginTop: -10,color:'black' }}>Địa điểm</Text>
+                            <Text style={{marginRight: 10,color:'black' }}>Địa điểm</Text>
                             
-                            <View style={{marginTop: -50, width: 230, height: 100 }}>
+                            <View style={{marginTop: -40, width: 230, height: 100 }}>
                                 <Dropdown 
                                     data={data}
                                     onChangeText={(valuePlacePhoto) => { valuePlacePhoto= this.setState({valuePlacePhoto}) }}
                                     />
                             </View>        
                         </View>
-
+                        {this.state.labelErrorTime === true ? 
+                             <View style={[stylesPostPhoto.title, {marginBottom: 20}]}>
+                                    <Text style={{color: 'red'}}>Bạn chưa chọn thời gian</Text>
+                             </View>: null}
                         <View style={stylesPostPhoto.title}>
                             <Text style ={{marginRight: 10, marginTop: -25, color:'black'}}>Thời gian từ</Text>
                             <DatePicker
@@ -137,10 +226,14 @@ export default class PostPhoto extends Component {
                                     onDateChange={(datetimePhoto1) => {this.setState({datetimePhoto1: datetimePhoto1})}}
                                 />
                         </View>
+                        {this.state.labelErrorCost === true ? 
+                             <View style={stylesPostPhoto.title}>
+                                    <Text style={{color: 'red'}}>Vui lòng nhập giá trị dương</Text>
+                             </View>: null}
                         <View style={stylesPostPhoto.title}>
-                            <Text style={{marginRight:10, marginTop: 30,color:'black' }}>Chi phí</Text>
-                            <TextInput  multiline={true} numberOfLines={10}  underlineColorAndroid='transparent'
-                                style={[stylesPostPhoto.txtPostPhoto,{ marginTop: -10}]} 
+                            <Text style={{marginRight:10, color:'black', marginTop: 15 }}>Tiền công</Text>
+                            <TextInput  multiline={true} 
+                                style={[stylesPostPhoto.txtCostPhoto,{ marginTop: -10}]} 
                                 onChangeText={(costPhoto) => this.setState({costPhoto})}>
                                 {this.state.costPhoto}</TextInput>
                         </View>
@@ -149,14 +242,7 @@ export default class PostPhoto extends Component {
                                 <Text style={{ textAlign:"center", color: 'white', marginTop: 5 }}>Gửi yêu cầu</Text>
                             </TouchableOpacity>
                             <TouchableOpacity style={stylesPostPhoto.txtBtnPostPhoto}
-                                    onPress={() => this.props.navigation.navigate('PostDetailPhoto',{
-                                        valueCategoryPhoto1: this.state.valueCategoryPhoto1,
-                                        contentPhoto: this.state.contentPhoto,
-                                        valuePlacePhoto: this.state.valuePlacePhoto,
-                                        datetimePhoto: this.state.datetimePhoto,
-                                        datetimePhoto1: this.state.datetimePhoto1,
-                                        costPhoto: this.state.costPhoto,
-                                    })}>
+                                    onPress={() => this.createPostPhoto()} >
                                 <Text style={{ textAlign:"center", color: 'white', marginTop: 5 }}>Tạo</Text>
                             </TouchableOpacity>
                         </View>
@@ -205,6 +291,9 @@ export default class PostPhoto extends Component {
             height: 100, width: 230,
             borderColor: 'black', 
             borderWidth: 1
+        },
+        txtCostPhoto: {
+             width: 230,
         },
         txtBtnPostPhoto: {
             width: 150, height: 30, borderRadius: 10, 
