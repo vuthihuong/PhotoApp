2cb712089b728b0fb8b66d8c280526ec8b9a56c1
vuthihuong <vuthihuong161996@gmail.com>
2018-12-11 11:22:54 +0700

    # them file list modal and event

---
+++ src/View/Post/PostModal/ListDirectPostModal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ScrollView, ListView
+} from 'react-native';
+
+export default class ListDirectPostModal extends Component {
+    render(){ 
+        return(
+            <View><Text>Hello World</Text></View>
+           
+        )
+    }
+}
+
